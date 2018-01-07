0517c0ac6bbcf55420d854d2aee3c2e739ed7b0e
Steven Inouye <steveninouye@msn.com>
2018-01-06 20:01:32 -1000

    dinosaur added

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -5,19 +5,40 @@ function makeElem(elem, label, inside){
     return container;
 }
 
-document.getElementById('pixelPainter').appendChild(makeElem('div', 'leftColumn', 'testing'));
-document.getElementById('pixelPainter').appendChild(makeElem('div', 'rightColumn', 'lorem ipsum stuf sfduaidio v naduvn adspvn asp'));
-
-document.getElementById('leftColumn').appendChild(makeElem('button', 'erase', 'Erase'));
-document.getElementById('leftColumn').appendChild(makeElem('button', 'clear', 'Clear'));
-
-function columnsAndRows(x, y){
+function mainColumnsAndRows(x, y){
     document.getElementById('rightColumn').appendChild(makeElem('table', 'mainTable', ''));
     for(var i = 0; i < x; i++){
         document.getElementById('mainTable').appendChild(makeElem('tr', 'row' + i, null));
         for(var h = 0; h < y; h++){
-            document.getElementById('row' + i).appendChild(makeElem('td', 'col' + h, h));
+            document.getElementById('row' + i).appendChild(makeElem('td', 'r' + i + 'c' + h, h));
         }
     }
 }
-columnsAndRows(10,20);
+
+function leftColumnsAndRows(x, y){
+    document.getElementById('leftColumn').appendChild(makeElem('table', 'colorPicker', ''));
+    for(var i = 0; i < x; i++){
+        document.getElementById('colorPicker').appendChild(makeElem('tr', 'roww' + i, null));
+        for(var h = 0; h < y; h++){
+            document.getElementById('roww' + i).appendChild(makeElem('td', 'r' + i + 'c' + h, h));
+        }
+    }
+}
+
+document.getElementById('pixelPainter').appendChild(makeElem('div', 'leftColumn', ''));
+document.getElementById('pixelPainter').appendChild(makeElem('div', 'rightColumn', ''));
+
+leftColumnsAndRows(10,5);
+
+document.getElementById('leftColumn').appendChild(makeElem('button', 'erase', 'Erase'));
+document.getElementById('leftColumn').appendChild(makeElem('button', 'clear', 'Clear'));
+
+
+mainColumnsAndRows(10,20);
+function makePicture(elem, label, source){
+    var container = document.createElement(elem);
+    container.id = label;
+    container.src = source;
+    return container;
+}
+document.querySelector("body").appendChild(makePicture('img', 'mainPic', '../dinosaur.jpg'))
