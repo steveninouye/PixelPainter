c0cd0200bf474c51aaa771147ca6cd2dcbd0b1b1
Steven Inouye <steveninouye@msn.com>
2018-01-09 00:56:18 -1000

    finished color pallet, erase button function, clear button function, and allowing user to color middle column table

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,7 +1,7 @@
-var colors = [
-    '#DCDCDC','#D3D3D3','#C0C0C0','#A9A9A9','#808080','#696969','#778899','#708090','#2F4F4F','#000000','#CD5C5C','#F08080','#FA8072','#E9967A','#FFA07A','#DC143C','#FF0000','#B22222','#8B0000','#FFC0CB','#FFB6C1','#FF69B4','#FF1493','#C71585','#DB7093','#FFA07A','#FF7F50','#FF6347','#FF4500','#FF8C00','#FFA500','#FFD700','#FFFF00','#FFFFE0','#FFFACD','#FAFAD2','#FFEFD5','#FFE4B5','#FFDAB9','#EEE8AA','#F0E68C','#BDB76B','#E6E6FA','#D8BFD8','#DDA0DD','#EE82EE','#DA70D6','#FF00FF','#FF00FF','#BA55D3','#9370DB','#663399','#8A2BE2','#9400D3','#9932CC','#8B008B','#800080','#4B0082','#6A5ACD','#483D8B','#7B68EE','#ADFF2F','#7FFF00','#7CFC00','#00FF00','#32CD32','#98FB98','#90EE90','#00FA9A','#00FF7F','#3CB371','#2E8B57','#228B22','#008000','#006400','#9ACD32','#6B8E23','#808000','#556B2F','#66CDAA','#8FBC8B','#20B2AA','#008B8B','#008080','#00FFFF','#00FFFF','#E0FFFF','#AFEEEE','#7FFFD4','#40E0D0','#48D1CC','#00CED1','#5F9EA0','#4682B4','#B0C4DE','#B0E0E6','#ADD8E6','#87CEEB','#87CEFA','#00BFFF','#1E90FF','#6495ED','#7B68EE','#4169E1','#0000FF','#0000CD','#00008B','#000080','#191970','#FFF8DC','#FFEBCD','#FFE4C4','#FFDEAD','#F5DEB3','#DEB887','#D2B48C','#BC8F8F','#F4A460','#DAA520','#B8860B','#CD853F','#D2691E','#8B4513','#A0522D','#A52A2A','#800000','#FFFFFF','#FFFAFA','#F0FFF0','#F5FFFA','#F0FFFF','#F0F8FF','#F8F8FF','#F5F5F5','#FFF5EE','#F5F5DC','#FDF5E6','#FFFAF0','#FFFFF0','#FAEBD7','#FAF0E6','#FFF0F5','#FFE4E1'
-]
+var colors = ["#ffffff","#ffebee","#ffcdd2","#ef9a9a","#e57373","#ef5350","#f44336","#e53935","#d32f2f","#c62828","#b71c1c","#ff8a80","#ff5252","#ff1744","#d50000","#000000","#ffffff","#fce4ec","#f8bbd0","#f48fb1","#f06292","#ec407a","#e91e63","#d81b60","#c2185b","#ad1457","#880e4f","#ff80ab","#ff4081","#f50057","#c51162","#000000","#ffffff","#f3e5f5","#e1bee7","#ce93d8","#ba68c8","#ab47bc","#9c27b0","#8e24aa","#7b1fa2","#6a1b9a","#4a148c","#ea80fc","#e040fb","#d500f9","#aa00ff","#000000","#ffffff","#ede7f6","#d1c4e9","#b39ddb","#9575cd","#7e57c2","#673ab7","#5e35b1","#512da8","#4527a0","#311b92","#b388ff","#7c4dff","#651fff","#6200ea","#000000","#ffffff","#e8eaf6","#c5cae9","#9fa8da","#7986cb","#5c6bc0","#3f51b5","#3949ab","#303f9f","#283593","#1a237e","#8c9eff","#536dfe","#3d5afe","#304ffe","#000000","#ffffff","#e3f2fd","#bbdefb","#90caf9","#64b5f6","#42a5f5","#2196f3","#1e88e5","#1976d2","#1565c0","#0d47a1","#82b1ff","#448aff","#2979ff","#2962ff","#000000","#ffffff","#e1f5fe","#b3e5fc","#81d4fa","#4fc3f7","#29b6f6","#03a9f4","#039be5","#0288d1","#0277bd","#01579b","#80d8ff","#40c4ff","#00b0ff","#0091ea","#000000","#ffffff","#e0f7fa","#b2ebf2","#80deea","#4dd0e1","#26c6da","#00bcd4","#00acc1","#0097a7","#00838f","#006064","#84ffff","#18ffff","#00e5ff","#00b8d4","#000000","#ffffff","#e0f2f1","#b2dfdb","#80cbc4","#4db6ac","#26a69a","#009688","#00897b","#00796b","#00695c","#004d40","#a7ffeb","#64ffda","#1de9b6","#00bfa5","#000000","#ffffff","#e8f5e9","#c8e6c9","#a5d6a7","#81c784","#66bb6a","#4caf50","#43a047","#388e3c","#2e7d32","#1b5e20","#b9f6ca","#69f0ae","#00e676","#00c853","#000000","#ffffff","#f1f8e9","#dcedc8","#c5e1a5","#aed581","#9ccc65","#8bc34a","#7cb342","#689f38","#558b2f","#33691e","#ccff90","#b2ff59","#76ff03","#64dd17","#000000","#ffffff","#f9fbe7","#f0f4c3","#e6ee9c","#dce775","#d4e157","#cddc39","#c0ca33","#afb42b","#9e9d24","#827717","#f4ff81","#eeff41","#c6ff00","#aeea00","#000000","#ffffff","#fffde7","#fff9c4","#fff59d","#fff176","#ffee58","#ffeb3b","#fdd835","#fbc02d","#f9a825","#f57f17","#ffff8d","#ffff00","#ffea00","#ffd600","#000000","#ffffff","#fff8e1","#ffecb3","#ffe082","#ffd54f","#ffca28","#ffc107","#ffb300","#ffa000","#ff8f00","#ff6f00","#ffe57f","#ffd740","#ffc400","#ffab00","#000000","#ffffff","#fff3e0","#ffe0b2","#ffcc80","#ffb74d","#ffa726","#ff9800","#fb8c00","#f57c00","#ef6c00","#e65100","#ffd180","#ffab40","#ff9100","#ff6d00","#000000","#ffffff","#fbe9e7","#ffccbc","#ffab91","#ff8a65","#ff7043","#ff5722","#f4511e","#e64a19","#d84315","#bf360c","#ff9e80","#ff6e40","#ff3d00","#dd2c00","#000000","#ffffff","#efebe9","#d7ccc8","#bcaaa4","#a1887f","#a1887f","#8d6e63","#795548","#795548","#6d4c41","#5d4037","#5d4037","#4e342e","#4e342e","#3e2723","#000000","#ffffff","#fafafa","#f5f5f5","#eeeeee","#e0e0e0","#bdbdbd","#9e9e9e","#9e9e9e","#757575","#757575","#616161","#616161","#424242","#424242","#212121","#000000","#ffffff","#eceff1","#eceff1","#cfd8dc","#b0bec5","#b0bec5","#90a4ae","#78909c","#78909c","#607d8b","#546e7a","#455a64","#455a64","#37474f","#263238","#000000"]
 
+//cache variables
+var colorPicked = "";
 
 
 function makeElem(elem, label, inside){
@@ -21,15 +21,17 @@ function mainColumnsAndRows(x, y){
     }
 }
 
-function colorPickerColumnsAndRows(x, y){
+function colorPickerRowsAndColumns(x, y){
     document.getElementById('leftColumn').appendChild(makeElem('table', 'colorPicker', ''));
     for(var i = 0; i < x; i++){
         document.getElementById('colorPicker').appendChild(makeElem('tr', 'roww' + i, null));
         for(var h = 0; h < y; h++){
-            document.getElementById('roww' + i).appendChild(makeElem('td', 'r' + i + 'c' + h, "."));
-            let index = i * x + h;
-            document.getElementById('r' + i + 'c' + h).style.backgroundColor = colors[index];
+            document.getElementById('roww' + i).appendChild(makeElem('td', 'r' + i + 'c' + h, "."))
         }
+    }
+    let cache = document.getElementById('colorPicker').getElementsByTagName('td');
+    for(var k = 0; k < cache.length; k++){
+        cache[k].style.backgroundColor = colors[k];
     }
 }
 
@@ -43,40 +45,45 @@ function makePicture(elem, label, source){
 document.getElementById('pixelPainter').appendChild(makeElem('div', 'leftColumn', ''));
 document.getElementById('pixelPainter').appendChild(makeElem('div', 'middleColumn', ''));
 document.getElementById('pixelPainter').appendChild(makeElem('div', 'rightColumn', 'PICK A PICTURE'));
->>>>>>> ec4f933af6638a0d3b15d7e12d35e125749ee637
 
-colorPickerColumnsAndRows(10,14);
+colorPickerRowsAndColumns(19,16);
 document.getElementById('leftColumn').appendChild(makeElem('div', 'selectedColor', 'COLOR'));
 document.getElementById('leftColumn').appendChild(makeElem('button', 'erase', 'Erase'));
 document.getElementById('leftColumn').appendChild(makeElem('button', 'clear', 'Clear'));
 
 
 mainColumnsAndRows(25,25);
->>>>>>> ec4f933af6638a0d3b15d7e12d35e125749ee637
 
-// document.querySelector('body').appendChild(makePicture('img', 'mainPic', '../dinosaur.jpg'));
 document.getElementById('middleColumn').style.backgroundImage = "url('dinosaur.jpg')";
 
 //CLICK FUNCTION//
 let middleTable = document.getElementById('mainTable');
 let cells = middleTable.getElementsByTagName('td'); 
-    for (var i = 0; i < cells.length; i++) { 
-        cells[i].onclick = function(){
-            console.log('color placed!');
-        };
-    }
+
+for (var h = 0; h < cells.length; h++) { 
+    cells[h].onclick = function(){
+        this.style.backgroundColor = colorPicked;
+    };
+}
+
+
 let leftTable = document.getElementById('colorPicker');
 let cellsTwo = leftTable.getElementsByTagName('td'); 
-    for (var i = 0; i < cellsTwo.length; i++) { 
-        cellsTwo[i].onclick = function(){
-            console.log('color picked!');
-        };
-    }
-    
-//RANDOM COLORS TEST//
-var cellColors = leftTable.getElementsByTagName('td'),
-    colors = ['000000','FF0000','00FF00','0000FF','FFFF00','00FFFF','FF00FF','C0C0C0'];
 
-for(var i = 0; i < cellColors.length; i++) {
-    cellColors[i].style.backgroundColor = '#' + colors[Math.floor(Math.random() * colors.length)];
+for (var i = 0; i < cellsTwo.length; i++) {
+    cellsTwo[i].onclick = function(){
+        colorPicked = this.style.backgroundColor;
+        document.getElementById('selectedColor').style.backgroundColor = colorPicked;
+    };
+}
+
+document.getElementById('erase').onclick = function(){
+    colorPicked = "";
+    document.getElementById('selectedColor').style.backgroundColor = colorPicked;
+}
+
+document.getElementById('clear').onclick = function(){
+    for(var j = 0; j < cells.length; j++){
+        cells[j].style.backgroundColor = "";
+    }
 }
